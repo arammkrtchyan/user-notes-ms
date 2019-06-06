706845fc434ce05c9f0d0605973769d308ff8f67
gg_aram <aram@greatergood.com>
2019-06-06 13:53:53 +0400

    Add notes controllers.

--- note-service/src/main/java/co/donebyme/microservices/notes/NotesServiceApplication.java
+++ note-service/src/main/java/co/donebyme/microservices/notes/NotesServiceApplication.java
@@ -2,12 +2,19 @@ package co.donebyme.microservices.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NotesServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotesServiceApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
---
+++ note-service/src/main/java/co/donebyme/microservices/notes/port/adapter/controller/NoteController.java
@@ -0,0 +1,44 @@
+package co.donebyme.microservices.notes.port.adapter.controller;
+
+import co.donebyme.microservices.notes.application.NoteApplicationService;
+import co.donebyme.microservices.notes.application.command.SubmitNoteCommand;
+import co.donebyme.microservices.notes.domain.model.note.Note;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Aram Mkrtchyan.
+ */
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+
+
+    private final NoteApplicationService noteApplicationService;
+
+    public NoteController(NoteApplicationService noteApplicationService) {
+        this.noteApplicationService = noteApplicationService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Note submitNote(@RequestBody SubmitNoteCommand command) {
+        return noteApplicationService.submitNote(command);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/{id}")
+    public Note findNote(@PathVariable String id) {
+        String userId = null;
+        return noteApplicationService.findById(userId, id);
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.POST)
+    public List<Note> notesOfUser(String userId) {
+        return noteApplicationService.notesOfAuthor(userId);
+    }
+
+}
